de6f8a6422472e9f810476290d1fe7d4122fa76f
Richard_Chen <ybc5276@psu.edu>
2025-03-30 21:04:15 -0400

    remove login page

--- Capstone-Project-frontend/tracking/urls.py
+++ Capstone-Project-frontend/tracking/urls.py
@@ -1,12 +1,10 @@
-# tracking/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login_view, name='login'),
+    path('', views.dashboard_view, name='dashboard'),  # now the root shows the dashboard
     path('dashboard/', views.dashboard_view, name='dashboard'),
     path('new-videos/', views.new_videos_view, name='new_videos'),
-    path('register/', views.register_view, name='register'),
     path('download-csv/', views.download_csv, name='download_csv'),
     path('livestream/', views.livestream_view, name='livestream'),
 ]
