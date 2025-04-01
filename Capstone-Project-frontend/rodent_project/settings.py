e6701f9bb1c1dfcd7286ac8d144f5b4a4407474f
Richard_Chen <ybc5276@psu.edu>
2025-04-01 16:29:03 -0400

    Update settings.py

--- Capstone-Project-frontend/rodent_project/settings.py
+++ Capstone-Project-frontend/rodent_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-y+7ki^xngo$zy)sbl$-h(mp9(higaz#6%k_rkxbj$rdb=-dbzw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.134.247.124', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['3.136.37.111', 'localhost', '127.0.0.1']
 
 
 # Application definition
