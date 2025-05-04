69d2fa89bb64ed98d6058019e82b3da12fbb2b4e
Bazil Akram <154655277+BazilAkram@users.noreply.github.com>
2025-05-04 00:52:11 -0400

    Deleted redundant files

--- Capstone-App/tracking/urls.py
+++ Capstone-App/tracking/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('dashboard/', views.dashboard_view, name='dashboard'),
     path('process/', views.process_view, name='process'),
     path('visualize/', views.visualize_view, name='visualize'),
-    path('run-visualize/', views.run_visualize, name='run_visualize'),
     path('process-video/', views.process_video, name='process_video'),
     path('check-task-status/', views.check_task_status, name='check_task_status'),
     path('download-result/', views.download_result, name='download_result'),
